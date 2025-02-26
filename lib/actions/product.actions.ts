e1ea7cbefa83b4517a8b156531c968c65e4d8e1a
Pace <pace1@science.regn.net>
2025-02-26 11:28:15 +0000

    section 3 lesson 25

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,11 @@
 'use server';
 
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get latest products
 export async function getLatestProducts() {
-    const prisma = new PrismaClient();
 
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
@@ -14,4 +13,11 @@ export async function getLatestProducts() {
     });
 
     return convertToPlainObject(data);
+}
+
+// Get single product by its slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: {slug: slug},
+    })
 }
