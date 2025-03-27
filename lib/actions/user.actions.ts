207768e4ffd91bd454a01b957a4a6aefb6455d30
Pace <pace1@science.regn.net>
2025-03-27 15:50:46 +0000

    sign in and sign out action

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { signInFormSchema } from "../validators";
+import { signIn, signOut } from "@/auth";
+
+// Sign in the user with credentials
+export async function signInWithCredentials(prevState: unknown, formData: FormData) {
+    try {
+        const user = signInFormSchema.parse({
+            email: formData.get("email"),
+            password: formData.get("password"),
+        });
+
+        await signIn('credentials', user);
+        
+        return { success: true, message: 'Signed in successfully' }
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+
+        return { success: false, message: 'Invlid email or password' }
+    }
+}
+
+// Sign out the user
+export async function signOutUser() {
+    await signOut();
+}
